196902a846503887caf920b29706c01aaab50e6e
yuukki1 <yuuk1loveme@outlook.com>
2026-03-31 16:59:11 +0800

    fix:修复了gRPC传递格式不一致导致连接不能成功建立的bug，修复了引用错误Dict导致键值Trace_id错误的bug，需要确定返回客户端Response的时机

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import sys
 import asyncio
 import logging
 import os
 from grpc import aio
+from pathlib import Path
 
 # 引入 Agent
 from app.agent.react_agent import SecurityReActAgent
+# 引入 gRPC Servicer
+pb2_path = str(Path(__file__).parent / "app" / "grpc_server" / "pb2")
+if pb2_path not in sys.path:
+    sys.path.append(pb2_path)
 from app.grpc_server.servicer import LogAnalyzerServicer
+from app.grpc_server.pb2 import insight_pb2_grpc
 
 Format = "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
 logging.basicConfig(level=logging.INFO, format=Format)
@@ -22,22 +29,22 @@ async def agent_consumer_worker(queue: asyncio.Queue, agent: SecurityReActAgent)
     while True:
         try:
             log_data = await queue.get()
-            logger.info(f"[Main] [开始分析] TraceID: {log_data['trace_id']} | IP: {log_data['ip']}")
+            logger.info(f"[Main] [开始分析] TraceID: {log_data['trace_id']} | IP: {log_data['source_ip']}")
 
             analysis_result = await agent.analyze_log(log_data)
 
             if analysis_result.get("is_attack"):
-                logger.warning(f"[Main] 【拦截告警】 发现攻击！ TraceID: {analysis_result['trace_id']} | 类型：{analysis_result['reason']}")
+                logger.warning(f"[Main] 【拦截告警】 发现攻击！ TraceID: {log_data['trace_id']} | 分数: {analysis_result['confidence']} | 类型：{analysis_result['reason']}")
             else:
-                logger.info(f"[Main] 【正常请求】 TraceID: {analysis_result['trace_id']} | 放行")
+                logger.info(f"[Main] 【正常请求】 TraceID: {log_data['trace_id']} | 放行")
             
             queue.task_done()
         except Exception as e:
             logger.error(f"[Main] Agent 消费者引擎异常: {e}")
 
 async def serve():
-    api_key = os.getenv("OPENAI_API_KEY","DEFAULT_KEY")
-    base_url = os.getenv("OPENAI_API_BASE_URL","https://api.chatgpt.com/v1")
+    api_key = os.getenv("OPENAI_API_KEY")
+    base_url = os.getenv("OPENAI_API_BASE_URL","https://api.deepseek.com/v1")
     model_name = os.getenv("OPENAI_MODEL_NAME","deepseek-chat")
 
     security_agent = SecurityReActAgent(api_key, base_url, model_name)
@@ -47,6 +54,7 @@ async def serve():
 
     grpc_server = aio.server()
     servicer = LogAnalyzerServicer(log_queue=log_queue)
+    insight_pb2_grpc.add_LogAnalyserServicer_to_server(servicer, grpc_server)
 
     grpc_listen_addr = "[::]:50051"
     grpc_server.add_insecure_port(grpc_listen_addr)
